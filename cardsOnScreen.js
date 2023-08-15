d4ebf5d2736b43d3369ea234189f3c61e771e06f
Barbara Millon <78863735+rielara@users.noreply.github.com>
2023-08-15 14:17:12 +0100

    added fischer yates shuffle algorithm, cards still not displaying properly

--- cardsOnScreen.js
+++ cardsOnScreen.js
@@ -1,44 +1,16 @@
-export const cardsOnScreen = [
+// 53 cards in this deck
+// check out Fischer Yates shuffle algorithm and see if you can implement it here if not keep looking for a way to implement it
 
+export const cardsOnScreen = [
     {
         name: "ace of spades",
         value: 1,
         imageUrl: "./cards/ace_of_spades.jpg"
     },
-    {   
-        name: "ace of diamonds",
-        value: 1,
-        imageUrl: "./cards/ace_of_diamonds.jpg"
-    },
     {
-        name: "ace of hearts",
-        value: 1,
-        imageUrl: "./cards/ace_of_hearts.jpg"   
-    },
-    {
-        name: "ace of clubs",
-        value: 1,
-        imageUrl: "./cards/ace_of_clubs.jpg"
-    },
-    {   
         name: "2 of spades",
         value: 2,
         imageUrl: "./cards/2_of_spades.jpg"
-    },
-    {
-        name: "2 of diamonds",
-        value: 2,
-        imageUrl: "./cards/2_of_diamonds.jpg"
-    },
-    {
-        name: "2 of hearts",
-        value: 2,
-        imageUrl: "./cards/2_of_hearts.jpg"
-    },
-    {
-        name: "2 of clubs",
-        value: 2,
-        imageUrl: "./cards/2_of_clubs.jpg"
     },
     {
         name: "3 of spades",
@@ -46,39 +18,9 @@ export const cardsOnScreen = [
         imageUrl: "./cards/3_of_spades.jpg"
     },
     {
-        name: "3 of diamonds",
-        value: 3,
-        imageUrl: "./cards/3_of_diamonds.jpg"
-    },
-    {
-        name: "3 of hearts",
-        value: 3,
-        imageUrl: "./cards/3_of_hearts.jpg"
-    },
-    {
-        name: "3 of clubs",
-        value: 3,
-        imageUrl: "./cards/3_of_clubs.jpg"
-    },
-    {
         name: "4 of spades",
         value: 4,
         imageUrl: "./cards/4_of_spades.jpg"
-    },
-    {
-        name: "4 of diamonds",
-        value: 4,
-        imageUrl: "./cards/4_of_diamonds.jpg"
-    },
-    {
-        name: "4 of hearts",
-        value: 4,
-        imageUrl: "./cards/4_of_hearts.jpg"
-    },
-    {
-        name: "4 of clubs",
-        value: 4,
-        imageUrl: "./cards/4_of_clubs.jpg"
     },
     {
         name: "5 of spades",
@@ -86,39 +28,9 @@ export const cardsOnScreen = [
         imageUrl: "./cards/5_of_spades.jpg"
     },
     {
-        name: "5 of diamonds",
-        value: 5,
-        imageUrl: "./cards/5_of_diamonds.jpg"
-    },
-    {
-        name: "5 of hearts",
-        value: 5,
-        imageUrl: "./cards/5_of_hearts.jpg"
-    },
-    {
-        name: "5 of clubs",
-        value: 5,
-        imageUrl: "./cards/5_of_clubs.jpg"
-    },
-    {
         name: "6 of spades",
         value: 6,
         imageUrl: "./cards/6_of_spades.jpg"
-    },
-    {
-        name: "6 of diamonds",
-        value: 6,
-        imageUrl: "./cards/6_of_diamonds.jpg"
-    },
-    {
-        name: "6 of hearts",
-        value: 6,
-        imageUrl: "./cards/6_of_hearts.jpg"
-    },
-    {
-        name: "6 of clubs",
-        value: 6,
-        imageUrl: "./cards/6_of_clubs.jpg"
     },
     {
         name: "7 of spades",
@@ -126,39 +38,9 @@ export const cardsOnScreen = [
         imageUrl: "./cards/7_of_spades.jpg"
     },
     {
-        name: "7 of diamonds",
-        value: 7,
-        imageUrl: "./cards/7_of_diamonds.jpg"
-    },
-    {
-        name: "7 of hearts",
-        value: 7,
-        imageUrl: "./cards/7_of_hearts.jpg"
-    },
-    {
-        name: "7 of clubs",
-        value: 7,
-        imageUrl: "./cards/7_of_clubs.jpg"
-    },
-    {
         name: "8 of spades",
         value: 8,
         imageUrl: "./cards/8_of_spades.jpg"
-    },
-    {
-        name: "8 of diamonds",
-        value: 8,
-        imageUrl: "./cards/8_of_diamonds.jpg"
-    },
-    {
-        name: "8 of hearts",
-        value: 8,
-        imageUrl: "./cards/8_of_hearts.jpg"
-    },
-    {
-        name: "8 of clubs",
-        value: 8,
-        imageUrl: "./cards/8_of_clubs.jpg"
     },
     {
         name: "9 of spades",
@@ -166,59 +48,14 @@ export const cardsOnScreen = [
         imageUrl: "./cards/9_of_spades.jpg"
     },
     {
-        name: "9 of diamonds",
-        value: 9,
-        imageUrl: "./cards/9_of_diamonds.jpg"
-    },
-    {
-        name: "9 of hearts",
-        value: 9,
-        imageUrl: "./cards/9_of_hearts.jpg"
-    },
-    {
-        name: "9 of clubs",
-        value: 9,
-        imageUrl: "./cards/9_of_clubs.jpg"
-    },
-    {
         name: "10 of spades",
         value: 10,
         imageUrl: "./cards/10_of_spades.jpg"
     },
     {
-        name: "10 of diamonds",
-        value: 10,
-        imageUrl: "./cards/10_of_diamonds.jpg"
-    },
-    {
-        name: "10 of hearts",
-        value: 10,
-        imageUrl: "./cards/10_of_hearts.jpg"
-    },
-    {
-        name: "10 of clubs",
-        value: 10,
-        imageUrl: "./cards/10_of_clubs.jpg"
-    },
-    {
         name: "jack of spades",
         value: 10,
-        imageUrl: "./cards/jack_of_spades.jpg"  
-    },
-    {
-        name: "jack of diamonds",
-        value: 10,
-        imageUrl: "./cards/jack_of_diamonds.jpg"
-    },
-    {
-        name: "jack of hearts",
-        value: 10,
-        imageUrl: "./cards/jack_of_hearts.jpg"
-    },
-    {
-        name: "jack of clubs",
-        value: 10,
-        imageUrl: "./cards/jack_of_clubs.jpg"
+        imageUrl: "./cards/jack_of_spades.jpg"
     },
     {
         name: "queen of spades",
@@ -226,9 +63,129 @@ export const cardsOnScreen = [
         imageUrl: "./cards/queen_of_spades.jpg"
     },
     {
+        name: "king of spades",
+        value: 10,
+        imageUrl: "./cards/king_of_spades.jpg"
+    },
+    {
+        name: "ace of diamonds",
+        value: 1,
+        imageUrl: "./cards/ace_of_diamonds.jpg"
+    },
+    {
+        name: "2 of diamonds",
+        value: 2,
+        imageUrl: "./cards/2_of_diamonds.jpg"
+    },
+    {
+        name: "3 of diamonds",
+        value: 3,
+        imageUrl: "./cards/3_of_diamonds.jpg"
+    },
+    {
+        name: "4 of diamonds",
+        value: 4,
+        imageUrl: "./cards/4_of_diamonds.jpg"
+    },
+    {
+        name: "5 of diamonds",
+        value: 5,
+        imageUrl: "./cards/5_of_diamonds.jpg"
+    },
+    {
+        name: "6 of diamonds",
+        value: 6,
+        imageUrl: "./cards/6_of_diamonds.jpg"
+    },
+    {
+        name: "7 of diamonds",
+        value: 7,
+        imageUrl: "./cards/7_of_diamonds.jpg"
+    },
+    {
+        name: "8 of diamonds",
+        value: 8,
+        imageUrl: "./cards/8_of_diamonds.jpg"
+    },
+    {
+        name: "9 of diamonds",
+        value: 9,
+        imageUrl: "./cards/9_of_diamonds.jpg"
+    },
+    {
+        name: "10 of diamonds",
+        value: 10,
+        imageUrl: "./cards/10_of_diamonds.jpg"
+    },
+    {
+        name: "jack of diamonds",
+        value: 10,
+        imageUrl: "./cards/jack_of_diamonds.jpg"
+    },
+    {
         name: "queen of diamonds",
         value: 10,
         imageUrl: "./cards/queen_of_diamonds.jpg"
+    },
+    {
+        name: "king of diamonds",
+        value: 10,
+        imageUrl: "./cards/king_of_diamonds.jpg"
+    },
+    {
+        name: "ace of hearts",
+        value: 1,
+        imageUrl: "./cards/ace_of_hearts.jpg"
+    },
+    {
+        name: "2 of hearts",
+        value: 2,
+        imageUrl: "./cards/2_of_hearts.jpg"
+    },
+    {
+        name: "3 of hearts",
+        value: 3,
+        imageUrl: "./cards/3_of_hearts.jpg"
+    },
+    {
+        name: "4 of hearts",
+        value: 4,
+        imageUrl: "./cards/4_of_hearts.jpg"
+    },
+    {
+        name: "5 of hearts",
+        value: 5,
+        imageUrl: "./cards/5_of_hearts.jpg"
+    },
+    {
+        name: "6 of hearts",
+        value: 6,
+        imageUrl: "./cards/6_of_hearts.jpg"
+    },
+    {
+        name: "7 of hearts",
+        value: 7,
+        imageUrl: "./cards/7_of_hearts.jpg"
+    },
+    {
+        name: "8 of hearts",
+        value: 8,
+        imageUrl: "./cards/8_of_hearts.jpg"
+    },
+    {
+        name: "9 of hearts",
+        value: 9,
+        imageUrl: "./cards/9_of_hearts.jpg"
+    },
+    {
+        name: "10 of hearts",
+        value: 10,
+        imageUrl: "./cards/10_of_hearts.jpg"
+    },
+    {
+        name: "jack of hearts",
+        value: 10,
+        imageUrl: "./cards/jack_of_hearts.jpg"
     },
     {
         name: "queen of hearts",
@@ -236,30 +193,73 @@ export const cardsOnScreen = [
         imageUrl: "./cards/queen_of_hearts.jpg"
     },
     {
-        name: "queen of clubs",
-        value: 10,
-        imageUrl: "./cards/queen_of_clubs.jpg"
-    },
-    {
-        name: "king of spades",
-        value: 10,
-        imageUrl: "./cards/king_of_spades.jpg"
-    },
-    {   
-        name: "king of diamonds",
-        value: 10,
-        imageUrl: "./cards/king_of_diamonds.jpg"
-    },
-    {
         name: "king of hearts",
         value: 10,
         imageUrl: "./cards/king_of_hearts.jpg"
     },
     {
+        name: "ace of clubs",
+        value: 1,
+        imageUrl: "./cards/ace_of_clubs.jpg"
+    },
+    {
+        name: "2 of clubs",
+        value: 2,
+        imageUrl: "./cards/2_of_clubs.jpg"
+    },
+    {
+        name: "3 of clubs",
+        value: 3,
+        imageUrl: "./cards/3_of_clubs.jpg"
+    },
+    {
+        name: "4 of clubs",
+        value: 4,
+        imageUrl: "./cards/4_of_clubs.jpg"
+    },
+    {
+        name: "5 of clubs",
+        value: 5,
+        imageUrl: "./cards/5_of_clubs.jpg"
+    },
+    {
+        name: "6 of clubs",
+        value: 6,
+        imageUrl: "./cards/6_of_clubs.jpg"
+    },
+    {
+        name: "7 of clubs",
+        value: 7,
+        imageUrl: "./cards/7_of_clubs.jpg"
+    },
+    {
+        name: "8 of clubs",
+        value: 8,
+        imageUrl: "./cards/8_of_clubs.jpg"
+    },
+    {
+        name: "9 of clubs",
+        value: 9,
+        imageUrl: "./cards/9_of_clubs.jpg"
+    },
+    {
+        name: "10 of clubs",
+        value: 10,
+        imageUrl: "./cards/10_of_clubs.jpg"
+    },
+    {
+        name: "jack of clubs",
+        value: 10,
+        imageUrl: "./cards/jack_of_clubs.jpg"
+    },
+    {
+        name: "queen of clubs",
+        value: 10,
+        imageUrl: "./cards/queen_of_clubs.jpg"
+    },
+    {
         name: "king of clubs",
         value: 10,
         imageUrl: "./cards/king_of_clubs.jpg"
-
     }
-
 ];
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { cardsOnScreen } from "./cardsOnScreen.js";
 
+const cards = Array.from({ length: 10 }, (_, i) => i + 1);
 let displayedCards = cardsOnScreen;
 
-let cards = [];
 let sum = 0;
 let hasBlackJack = false;
 let isAlive = false;
@@ -16,23 +16,33 @@ document.getElementById("start-btn").addEventListener("click", startGame);
 document.getElementById("new-card-btn").addEventListener("click", newCard);
 
 let player = {
-    name: "Your name",
+    name: "Player",
     chips: 145
 };
 
 playerEl.textContent = player.name + ": $" + player.chips;
 
-// shuffle cards instead and take first instead 
-function getRandomCard() {
-    let randomNumber = Math.floor(Math.random() * 13) + 1;
-    if (randomNumber > 10) {
-        return 10;
-    } else if (randomNumber === 1) {
-        return 11;
-    } else {
-        return randomNumber;
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
+
+shuffleArray(displayedCards);
+
+// shuffle cards and for each shuffling take one card from the shuffled array
+// see Fischer Yates shuffle algorithm and see if you can implement it here if not keep looking for a way to implement it
+function getRandomCard() {
+    if (displayedCards.length === 0) {
+        shuffleArray(cards);
+        displayedCards = [...cardsOnScreen];
+    }
+
+    const card = displayedCards.pop();
+    return card.value;
+}
+
 
 function startGame() {
     if (player.chips < 50) {
@@ -40,50 +50,56 @@ function startGame() {
         messageEl.textContent = message;
         return;
     }
-    
+
     isAlive = true;
-    hasBlackJack = false; // Reset the hasBlackJack flag
+    hasBlackJack = false;
+    cards.length = 0;
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
-    cards = [firstCard, secondCard];
+    cards.push(firstCard, secondCard);
     sum = firstCard + secondCard;
     renderGame();
 }
 
+
 function renderGame() {
-    cardsEl.innerHTML = ""; // this resets the cards 
+    cardsEl.innerHTML = "";
 
     for (let i = 0; i < cards.length; i++) {
-        let cardIndex = cards[i] - 1; // Subtract 1 to get the correct index
+        let cardIndex = cards[i] - 1;
         let cardImage = document.createElement("img");
         cardImage.classList.add("card-image");
         cardImage.setAttribute("src", displayedCards[cardIndex].imageUrl);
         cardsEl.appendChild(cardImage);
     }
 
-    sumEl.textContent = "Sum: " + sum;
-    if (sum <= 20) {
+    // Calculate the displayed sum by subtracting the initial 10 from the total sum
+    let displayedSum = sum;
+    sumEl.textContent = "Sum: " + displayedSum;
+
+    if (sum < 21) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
         message = "You've got Blackjack!";
         hasBlackJack = true;
-        player.chips += 10; 
+        player.chips += 10;
     } else {
         message = "You're out of the game!";
         isAlive = false;
         if (!hasBlackJack) {
-            player.chips -= 10; 
+            player.chips -= 10;
         }
     }
-    
+
     if (player.chips <= 0) {
         message = "You're out of chips, sorry!";
         isAlive = false;
     }
-    
+
     messageEl.textContent = message;
     playerEl.textContent = player.name + ": $" + player.chips;
 }
+
 
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
@@ -92,7 +108,7 @@ function newCard() {
             messageEl.textContent = message;
             return;
         }
-        
+
         let card = getRandomCard();
         sum += card;
         cards.push(card);
